1609339a9f4df58c994abeb8b222b4be08f7b0ad
ncmarte <ncmarte@itpi.com.ph>
2023-05-08 09:21:43 +0800

    finished module 2

--- app.js
+++ app.js
@@ -1,72 +1,104 @@
 (function () {
 'use strict';
 
-var shoppingList = [
-    {
-    name: "Milk",
-    quantity: 4
-    },
-    {
-        name: "Chocolate",
-        quantity: 53
-    },
-    {
-        name: "Cookies",
-        quantity: 43
-    },
-    {
-        name: "Smoothies",
-        quantity: 31
-    },
-    {
-        name: "Cake",
-        quantity: 10
-    },
-    {
-        name: "Milktea",
-        quantity: 24
-    },
-    {
-        name: "Shabu-shabu",
-        quantity: 23
-    },
-    {
-        name: "Lemonade",
-        quantity: 20
-    },
-    {
-        name: "Coffee",
-        quantity: 20
-    },
-    {
-        name: "Cheese Cake",
-        quantity: 20
-    }
-    ];
 
-    var soldList = 
-    [{}];
+
+    
 
     angular.module('module2App',[])
     .controller('toBuyController', toBuyController)
-    .controller('boughtController', boughtController);
+    .controller('boughtController', boughtController)
+    .service('shoppingListCheckoffService',shoppingListCheckoffService);
 
-    toBuyController.$inject=['$scope'];
-    function toBuyController($scope)
+    toBuyController.$inject=['shoppingListCheckoffService'];
+    function toBuyController(shoppingListCheckoffService)
     {
-        $scope.shop = shoppingList;
-       $scope.addBought = function(){
+        var tobuy = this;
+        tobuy.addShoptoSold = function(index){
+            shoppingListCheckoffService.addShoptoSold(index);
+            
+        }
+        tobuy.getTobuyList = shoppingListCheckoffService.getTobuyList();
 
-            return console.log("To do");
 
-       }
-        
     }
-    boughtController.$inject=['$scope'];
-    function boughtController($scope)
+    boughtController.$inject=['shoppingListCheckoffService'];
+    function boughtController(shoppingListCheckoffService)
     {
-        $scope.sold = soldList;
+        var toSold = this;
+        toSold.getSoldList = shoppingListCheckoffService.getSoldList();
        
-        
     }
+    function shoppingListCheckoffService()
+    {
+        var service = this;
+
+        var shoppingList = [
+            {
+            name: "Milk",
+            quantity: 4
+            },
+            {
+                name: "Chocolate",
+                quantity: 53
+            },
+            {
+                name: "Cookies",
+                quantity: 43
+            },
+            {
+                name: "Smoothies",
+                quantity: 31
+            },
+            {
+                name: "Cake",
+                quantity: 10
+            },
+            {
+                name: "Milktea",
+                quantity: 24
+            },
+            {
+                name: "Shabu-shabu",
+                quantity: 23
+            },
+            {
+                name: "Lemonade",
+                quantity: 20
+            },
+            {
+                name: "Coffee",
+                quantity: 20
+            },
+            {
+                name: "Cheese Cake",
+                quantity: 20
+            }
+            ];
+
+            var soldList = 
+             [{}];
+
+            service.addShoptoSold = function(index,cnt)
+            {
+                var shopListItems = shoppingList[index];
+                var item = {
+                    name : shopListItems.name, 
+                    quantity : shopListItems.quantity
+                };
+                soldList.push(item);
+                shoppingList.splice(index,1);
+            }
+
+            service.getTobuyList = function()
+            {
+                return shoppingList;
+            }
+            service.getSoldList = function()
+            {
+                return soldList;
+            }
+            
+    }
+
 })();
